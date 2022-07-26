7b6ee5e1b6dffd4a41dc7c072c6443d2ff66f7ac
Evanl <Evanlou050102@gmail.com>
2022-07-26 15:44:36 -0700

    1

---
+++ main.py
@@ -0,0 +1,57 @@
+import cv2
+import serial
+import imutils
+import tkinter as tk
+
+# set background as none
+bg = None
+
+
+if __name__ == '__main__':
+    # get the reference to the webcam
+    camera = cv2.VideoCapture(0)
+
+    # region of interest (ROI) coordinates
+    top, right, bottom, left = 10, 350, 225, 590
+    while (True):
+        # get the current frame
+        (grabbed, frame) = camera.read()
+
+        # resize the frame
+        frame = imutils.resize(frame, width=700)
+
+        # flip the frame so that it is not the mirror view
+        frame = cv2.flip(frame, 1)
+
+        # clone the frame
+        clone = frame.copy()
+
+        # get the height and width of the frame
+        (height, width) = frame.shape[:2]
+
+        # get the ROI
+        roi = frame[top:bottom, right:left]
+
+        # convert the roi to grayscale and blur it
+        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+        # to get the background, keep looking till a threshold is reached
+        # so that our running average model gets calibrated
+
+
+        cv2.imshow("Video Feed", clone)
+
+        # observe the keypress by the user
+        keypress = cv2.waitKey(1) & 0xFF
+
+        # if the user pressed "q", then stop looping
+        if keypress == ord("q"):
+            break
+        if keypress == ord("r"):
+            num_frames = 0
+            bg = None
+            done = False
+
+# free up memory
+cv2.destroyAllWindows()
